339c567d7850dd11a011a79c4d3a6943626c8eb2
Krish <154747008+krishhari2105@users.noreply.github.com>
2026-06-02 09:06:53 +0530

    Update build_from_repo.py

--- scripts/build_from_repo.py
+++ scripts/build_from_repo.py
@@ -13,13 +13,6 @@ APK_REPO_OWNER = "krishhari2105"
 APK_REPO_NAME = "base-apks"
 
 SOURCES = {
-    "anddea": {
-        "patches_repo": "anddea/revanced-patches",
-        "cli_repo": "MorpheApp/morphe-cli", 
-        "patches_asset": ".mpp",
-        "cli_asset": ".jar",
-        "use_prerelease": False
-    },
     "morphe": {
         "patches_repo": "MorpheApp/morphe-patches",
         "cli_repo": "MorpheApp/morphe-cli",
